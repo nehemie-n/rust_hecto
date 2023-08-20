9a60f8289dd2a497a81f4e1153da2c04f679933d
nehemie-n <nehemie054@gmail.com>
2023-08-20 04:13:01 +0200

    feat: switched to using a structure in module

---
+++ src/editor.rs
@@ -0,0 +1,31 @@
+use std::io::{self, stdout};
+use termion::event::Key;
+use termion::input::TermRead;
+use termion::raw::IntoRawMode;
+
+
+fn die(e: std::io::Error) {
+    panic!("{:?}", e);
+}
+
+pub struct Editor {}
+
+impl Editor {
+    pub fn run(&self) {
+        let _stdout = stdout().into_raw_mode().unwrap();
+
+        for key in io::stdin().keys() {
+            match key {
+                Ok(key) => match key {
+                    Key::Char(c) => {
+                        println!("{}\r", c);
+                    }
+                    Key::Ctrl('q') => break,
+                    Key::Esc => break,
+                    _ => println!("{:?}\r", key),
+                },
+                Err(err) => die(err),
+            }
+        }
+    }
+}
