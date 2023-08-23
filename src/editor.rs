b787ebd0f41040e78f6e1e8d1911f41aa991adba
nehemie-n <nehemie054@gmail.com>
2023-08-23 14:45:19 +0200

    feat: stdout().flush

--- src/editor.rs
+++ src/editor.rs
@@ -1,4 +1,4 @@
-use std::io::{self, stdout, Error};
+use std::io::{self, stdout, Error, Write};
 use termion::event::Key;
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
@@ -7,38 +7,52 @@ fn die(e: &Error) {
     panic!("{e:?}");
 }
 
-pub struct Editor {}
+pub struct Editor {
+    should_quit: bool,
+}
 
 impl Editor {
     pub fn default() -> Self {
-        Self {}
+        Self { should_quit: false }
     }
 
-    pub fn run(&self) {
+    pub fn run(&mut self) {
         let _stdout = stdout().into_raw_mode().unwrap();
         loop {
+            if let Err(error) = self.refresh_screen() {
+                die(&error);
+            }
+            if self.should_quit {
+                break;
+            }
             if let Err(error) = self.process_keypress() {
                 die(&error);
             }
         }
     }
 
-    fn process_keypress(&self) -> Result<(), std::io::Error> {
-        let pressed_key = self.read_key()?;
+    fn refresh_screen(&self) -> Result<(), std::io::Error> {
+        print!("\x1b[2J");
+        io::stdout().flush()
+    }
+
+    fn process_keypress(&mut self) -> Result<(), std::io::Error> {
+        let pressed_key = Editor::read_key()?;
         match pressed_key {
-            Key::Ctrl('q') | Key::Esc => panic!("Program End"),
+            Key::Ctrl('q') | Key::Esc => self.should_quit = true,
+            Key::Char(_char) => {
+                println!("{_char:?}")
+            }
             _ => (),
         };
         Ok(())
     }
 
-    fn read_key(&self) -> Result<Key, std::io::Error> {
+    fn read_key() -> Result<Key, std::io::Error> {
         loop {
             if let Some(key) = io::stdin().lock().keys().next() {
                 return key;
             }
         }
     }
-    
 }
-
